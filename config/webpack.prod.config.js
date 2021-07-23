4c8bf7a3502d4657eba33fe038987306f9eaab2a
多喝热水长生不老 <7804417+william408@user.noreply.gitee.com>
2021-07-23 20:10:15 +0800

    生产环境打包

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
 
@@ -13,5 +16,79 @@ module.exports = {
   output: {
     path: path.join(__dirname,'../build'),
     filename: '[name].js'
-  }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "less-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          }
+        ]
+      },
+      {
+        test:/\.(png|jpg|jpeg|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 8192
+          }
+        }
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    // 自动生成html，并自动将打包后资源引入
+    new HtmlWebpackPlugin({
+      template: "./src/assets/index.html",
+      minify: {
+        collapseWhitespace: true
+      }
+    }),
+
+    new MiniCssExtractPlugin({
+      moduleFilename: ({name}) => `${name.replace('js', 'css')}-[hash].css`,
+      chunkFilename: "[id].css"
+    }),
+
+    new OptimizeCssAssetsPlugin()
+  ]
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./assets/common.less";
 console.log(1231231231)
-
-const png = require("./assets/img/v2-c16278ddebeedc4a64912e527cf7fb45_b.jpg").default
-console.log(png)
-document.querySelector("#sdk-app").innerHTML = `<img src = ${png} alt=""/>`
 ReactDom.render(<div>1231231231</div>, document.getElementById("sdk-app"));
